c9207fc2f77fa22ccca057bd78e85ca865354a15
David Mackay <67295991+david-mackay@users.noreply.github.com>
2021-07-18 10:54:44 -0500

    Add files via upload

---
+++ Assets/ChangeFace.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeFace : MonoBehaviour
+{
+    public Texture sadface;
+    public Texture happyface;
+    public Texture neutralface;
+    public Texture excitedface;
+    public Renderer rend;
+
+    void Start()
+    {
+       
+    }
+
+    public void RendSadFace()
+    {
+        //rend = GetComponent<Renderer>();
+        rend.material.mainTexture = sadface;
+    }
+
+    public void RendHappyFace()
+    {
+        rend.material.mainTexture = happyface;
+    }
+
+    public void RendNeutralFace()
+    {
+        rend.material.mainTexture = neutralface;
+    }
+
+    public void RendExcitedFace()
+    {
+        rend.material.mainTexture = excitedface;
+    }
+    
+}
---
+++ Assets/Disappear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    public bool disappear;
+    public GameObject furniture;
+
+    
+    // Use this for initialization
+    void Start()
+    {
+        furniture.SetActive(true);
+    }
+
+    public void SetInactive()
+    {
+        furniture.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
